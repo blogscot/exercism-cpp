63af19213c35299ce4bc44b99b80168739c9e6e3
Iain Diamond <iain.diamond@gmail.com>
2018-10-31 15:02:09 +0000

    Refactor switch cases

--- trinary/trinary.h
+++ trinary/trinary.h
@@ -11,13 +11,10 @@ int to_decimal(std::string text) {
   for (auto& chr : text) {
     result *= 3;
     switch (chr) {
-      case '1':
-        result += 1;
-        break;
       case '2':
-        result += 2;
-        break;
+      case '1':
       case '0':
+        result += chr - '0';
         break;
       default:
         return 0;

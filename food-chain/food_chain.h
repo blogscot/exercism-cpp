16eff3fa51056250ba721e0786ae2da373edf28c
Iain Diamond <iain.diamond@gmail.com>
2018-11-09 13:37:24 +0000

    Add Food Chain solution

---
+++ food-chain/food_chain.h
@@ -0,0 +1,14 @@
+#ifndef FOOD_CHAIN_H_
+#define FOOD_CHAIN_H_
+
+#include <string>
+
+namespace food_chain {
+
+std::string verse(uint8_t);
+std::string verses(uint8_t, uint8_t);
+std::string sing();
+
+}  // namespace food_chain
+
+#endif  // FOOD_CHAIN_H_

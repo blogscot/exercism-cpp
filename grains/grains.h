82a474a6e83f395f96e6cd07e69d95f6739e89f1
Iain Diamond <iain.diamond@gmail.com>
2018-11-10 18:09:29 +0000

    Fix: float -> uint64_t

--- grains/grains.h
+++ grains/grains.h
@@ -1,6 +1,7 @@
 #ifndef GRAINS_H_
 #define GRAINS_H_
 
+#include <cassert>
 #include <cmath>
 #include <cstdint>
 
@@ -8,8 +9,13 @@ namespace grains {
 
 const int NUM_SQUARES = 64;
 
-auto square = [](uint32_t value) -> float { return std::pow(2, value - 1); };
-auto total = []() -> float { return std::pow(2, NUM_SQUARES) - 1; };
+auto square = [](uint8_t value) -> uint64_t {
+  assert(value >= 1 && value <= 64);
+  return static_cast<u_int64_t>(std::pow(2, value - 1));
+};
+auto total = []() -> uint64_t {
+  return static_cast<u_int64_t>(std::pow(2, NUM_SQUARES)) - 1;
+};
 
 }  // namespace grains
 
